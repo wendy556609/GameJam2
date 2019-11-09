e0b745a83eadeea54c150bebbe1ed941f4dd8375
peggy75918 <57534354+peggy75918@users.noreply.github.com>
2019-11-09 11:06:41 +0800

    add npc behauior

---
+++ Assets/Scripts/NpcBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcBehavior : MonoBehaviour {
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetPosition = target.transform.position;
+        Vector3 position = transform.position;
+        targetPosition-position
+
+    }
+}
