4664f4ff04bbf1682777ac9a9b431c1b31f045ff
wendy <wendy556609@gmail.com>
2019-11-09 21:36:55 +0800

    Player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 public class Player : MonoBehaviour {
-	
+	public Wood wood;
 	Rigidbody2D playerRigidbody;
 	public GhostChoose ghostChoose;
 	IPlayer IPlayer;
 
 	//數值
+	public float backX=-2;
 	public float moveSpeed=3f;
 	public Vector2 transformValue;
 	public Vector3 RotateformValue;
 	public int step = 5;
 	private int GhostNum;
-	
+	float timer=0;
 	//按鍵
 	private float keyVertical;
     private float keyHorizontal;
@@ -22,12 +23,14 @@ public class Player : MonoBehaviour {
 	public bool isStop=false;
 	public bool isGhost=false;
 	public bool isWalk=false;
+	bool isMove=false;
 	public string GetPlayerInputString;
 	// Use this for initialization
 	void Awake(){
 	}
 	
 	void Start () {
+		wood=GameObject.Find("Wood").GetComponent<Wood>();
 		IPlayer=GameObject.Find("IPlayer").GetComponent<IPlayer>();
 		playerRigidbody=GetComponent<Rigidbody2D>();
 		if(this.name==("Player"+ghostChoose.GetNum())){
@@ -48,23 +51,53 @@ public class Player : MonoBehaviour {
 			}
 		}
 
+		
+
 		if(!isStop){
-			GetKey();
+			GetKey();	
+			Punish();
 		}
 		else playerRigidbody.velocity= new Vector2(0.0f, 0.0f);
-	}
-
-	void GetKey(){
-		keyHorizontal=Input.GetAxisRaw(GetPlayerInputString+"_RIGHT");
-		keyVertical=Input.GetAxisRaw(GetPlayerInputString+"_UPDOWN");
 
 		if(keyHorizontal == 0 && keyVertical == 0){
 			isWalk=false;
 		}
 		else isWalk=true;
+		
+		
+		// if(isWalk&&wood.GetState==WoodState.STOP)
+	}
+	void Punish(){
+		if(isWalk&&wood.GetState==WoodState.STOP){
+			if(this.tag=="Player"){
+			timer+=1;
+			isMove=true;
+			if(timer<=1){
+				isMove=true;		
+				}	
+			else {
+				isMove=false;				
+			}	
+			if(isMove){
+			playerRigidbody.velocity = new Vector2(-10,0);
+			}
+			else playerRigidbody.velocity = new Vector2(0,0);
+			}
+		}
+		if(wood.GetState==WoodState.IDLE){
+			if(this.tag=="Player"){
+			isStop=false;
+			isMove=false;
+			}
+		}
+	}
+	void GetKey(){
+		keyHorizontal=Input.GetAxisRaw(GetPlayerInputString+"_RIGHT");
+		keyVertical=Input.GetAxisRaw(GetPlayerInputString+"_UPDOWN");
 
 		transformValue = new Vector2(keyHorizontal * Time.deltaTime * moveSpeed, keyVertical * Time.deltaTime * moveSpeed);
 		playerRigidbody.velocity = transformValue;   
+		
 		Walk();
 	}
 
