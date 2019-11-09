c8a35491cfc466bde36ba761632d6ba25b8c108e
tomray6971 <57533584+tomray6971@users.noreply.github.com>
2019-11-09 20:46:38 +0800

    Switch Scene

---
+++ Assets/Scripts/SB.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SB : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+   public void sb()
+    {
+        SceneManager.LoadScene("Main");//1是遊戲場景名稱
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+   
+        }
+
+ }
